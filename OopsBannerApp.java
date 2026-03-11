8e08129f948519ad4d432322531b4c4b5ea662cf
Aamir2426 <ak7986@srmist.edu.in>
2026-03-11 15:05:24 +0530

    Added-uc2

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -2,8 +2,13 @@ public class OopsBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" OOPS ");
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *         ***** ");
 
     }
 }
-
