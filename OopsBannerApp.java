97bb7aa8b4636564d9b0c565b2a676c079cbf073
Aamir2426 <ak7986@srmist.edu.in>
2026-03-11 16:01:01 +0530

    added UC6

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,54 +1,52 @@
 public class OopsBannerApp {
 
-public static void main(String[] args) {
-
-        String[] banner = {
-
-            String.join("  ",
-                    " ***** ",
-                    " ***** ",
-                    " ******",
-                    " ***** "),
-
-            String.join("  ",
-                    "*     *",
-                    "*     *",
-                    "*     *",
-                    "*      "),
-
-            String.join("  ",
-                    "*     *",
-                    "*     *",
-                    "*     *",
-                    "*      "),
-
-            String.join("  ",
-                    "*     *",
-                    "*     *",
-                    "****** ",
-                    " ***** "),
-
-            String.join("  ",
-                    "*     *",
-                    "*     *",
-                    "*      ",
-                    "      *"),
-
-            String.join("  ",
-                    "*     *",
-                    "*     *",
-                    "*      ",
-                    "      *"),
-
-            String.join("  ",
-                    " ***** ",
-                    " ***** ",
-                    "*      ",
-                    " ***** ")
+public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
         };
+    }
 
-        for (String line : banner) {
-            System.out.println(line);
+    public static String[] getP() {
+        return new String[]{
+                " ******",
+                "*     *",
+                "*     *",
+                " ******",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+   public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O1 = getO();
+        String[] O2 = getO();
+        String[] P  = getP();
+        String[] S  = getS();
+
+       
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O1[i] + "   " + O2[i] + "   " + P[i] + "   " + S[i]);
         }
     }
 }
+
