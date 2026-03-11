71154fa98fc067b0f65e1ed50009fdbb7248f344
Aamir2426 <ak7986@srmist.edu.in>
2026-03-11 15:56:30 +0530

    added UC5

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -2,53 +2,51 @@ public class OopsBannerApp {
 
 public static void main(String[] args) {
 
-        // Create array to store banner lines
         String[] banner = {
 
-            String.join(" ",
+            String.join("  ",
                     " ***** ",
                     " ***** ",
                     " ******",
                     " ***** "),
 
-            String.join(" ",
+            String.join("  ",
                     "*     *",
                     "*     *",
                     "*     *",
                     "*      "),
 
-            String.join(" ",
+            String.join("  ",
                     "*     *",
                     "*     *",
                     "*     *",
                     "*      "),
 
-            String.join(" ",
+            String.join("  ",
                     "*     *",
                     "*     *",
-                    " ******",
+                    "****** ",
                     " ***** "),
 
-            String.join(" ",
+            String.join("  ",
                     "*     *",
                     "*     *",
                     "*      ",
-                    "     * "),
+                    "      *"),
 
-            String.join(" ",
+            String.join("  ",
                     "*     *",
                     "*     *",
                     "*      ",
-                    "*    * "),
+                    "      *"),
 
-            String.join(" ",
+            String.join("  ",
                     " ***** ",
                     " ***** ",
                     "*      ",
                     " ***** ")
         };
 
-        // Print using enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
